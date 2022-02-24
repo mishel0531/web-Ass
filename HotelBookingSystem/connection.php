bcd67acd453bb393aacc8a22153c017bd42e30ac
RIDMIKW <ridmikawya321@gmail.com>
2022-02-24 12:37:20 +0700

    Create connection.php

---
+++ HotelBookingSystem/connection.php
@@ -0,0 +1,13 @@
+<?php
+    $servername = "localhost";
+    $username = "phpmyadmin";
+    $password = "phpmyadmin";
+    $dbname = "hotel_booking_system";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+?>
